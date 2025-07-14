0306dc56865e81f778f6319b70928921c9b78fb4
tyowk <tyowk@outlook.com>
2025-07-14 04:31:14 +0000

    *

--- commands/client/music/nowplaying.js
+++ commands/client/music/nowplaying.js
@@ -8,6 +8,8 @@ $image[$songInfo[thumbnail]]
 $title[Now Playing $songInfo[title];$songInfo[url]]
 $footer[$humanizeMs[$currentTrackDuration] / $songInfo[duration]  |  $songInfo[requester.username]]
 $cooldown[3s;{newEmbed:{description:Oops... Try again in %time%}}{deleteIn:5s}{suppress}]
+$onlyIf[$playerStatus!=destroyed;{newEmbed:{description:No tracks are currently playing!}}{deleteIn:5s}{suppress}]
+$onlyIf[$hasPlayer==true;{newEmbed:{description:There are no active players on this server!}}{deleteIn:5s}{suppress}]
 $suppressErrors
 `
 };
--- commands/client/music/resume.js
+++ commands/client/music/resume.js
@@ -9,6 +9,7 @@ $description[Resumed the player]
 $resumeTrack
 $endif
 $cooldown[3s;{newEmbed:{description:Oops... Try again in %time%}}{deleteIn:5s}{suppress}]
+$onlyIf[$playerStatus!=destroyed;{newEmbed:{description:No tracks are currently playing!}}{deleteIn:5s}{suppress}]
 $onlyIf[$voiceId==$voiceId[$clientId];{newEmbed:{description:You are not in the same voice channel!}}{deleteIn:5s}{suppress}]
 $onlyIf[$voiceId!=;{newEmbed:{description:You are not in a voice channel!}}{deleteIn:5s}{suppress}]
 $onlyIf[$hasPlayer==true;{newEmbed:{description:There are no active players on this server!}}{deleteIn:5s}{suppress}]
